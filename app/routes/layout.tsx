05078de8fa2fb764a75e5c0dcba3c81385070d97
Farjad <farjadabbas12@gmail.com>
2025-08-27 01:30:14 +0500

    .

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -6,9 +6,9 @@ import "@fontsource-variable/open-sans";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen">
+    <div>
       <Navbar />
-      <main className="py-15 relative z-10">
+      <main className="py-15 relative z-10 min-h-screen">
         <Outlet />
       </main>
       <footer className="bg-rose-950 opacity-85 backdrop-blur-xl p-4 text-white text-center relative z-2">
