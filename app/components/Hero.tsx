771eba2bceba1b5fd473277666a15d2ab77e6f40
Farjad <farjadabbas12@gmail.com>
2025-08-27 17:59:48 +0500

    Added a countdown in hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,11 @@
 import logo from "../assets/logo.jpeg";
 import Button from "./Button";
+import Countdown from "./Countdown";
 
 function Hero() {
   return (
     <article className="flex flex-col justify-center items-center pt-10 px-5 gap-y-8">
-      <div className="max-w-[350px] w-[40vw] min-w-[200px] lg:max-w-[420px] rounded-sm overflow-hidden animate-shadowGlow">
+      <div className="max-w-[350px] w-[40vh] min-w-[200px] lg:max-w-[420px] rounded-sm overflow-hidden animate-shadowGlow">
         <img src={logo} alt="UCL SportsFest" />
       </div>
       <div className="text-center">
@@ -17,6 +18,9 @@ function Hero() {
             25 <span className="">•</span> 26 <span className="">•</span> 27
           </div>
         </h1>
+        <div className="mt-3 mb-5">
+          <Countdown />
+        </div>
         <div className="my-4">
           <Button>Register</Button>
         </div>
--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -6,12 +6,12 @@ import "@fontsource-variable/open-sans";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen relative">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="pt-15 pb-20 relative z-10">
+      <main className="pt-15 pb-10 relative z-10 flex-1">
         <Outlet />
       </main>
-      <footer className="bg-rose-950 opacity-85 backdrop-blur-xl p-4 text-white text-center absolute bottom-0 right-0 left-0 z-2">
+      <footer className="bg-rose-950 opacity-85 backdrop-blur-xl p-4 text-white text-center z-2">
         &copy; {new Date().getFullYear()} UCL SportsFest. All rights reserved.
       </footer>
       <HeroBg />
