0d143099b8303c1a2ee22d27fb8ac0585183a63c
Farjad <farjadabbas12@gmail.com>
2025-08-28 09:00:08 +0500

    .

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -8,17 +8,46 @@ function contact() {
         contact us
       </h1>
       <div className="flex flex-col justify-evenly items-center mt-5 gap-y-8">
-        {data.map((d) => (
+        {/* {data.map((d) => (
           <ContactCard
             name={d.name}
             position={d.position}
             phone={d.phone}
             mail={d.mail}
           />
-        ))}
-        {/* <h2 className="text-slate-50 text-lg font-medium tracking-wide">
-          Aarij Waheed
-        </h2> */}
+        ))} */}
+        <div>
+          <div className="py-3 px-4 rounded-sm">
+            <h2 className="text-slate-50 text-xl font-medium tracking-wide">
+              Aarij Waheed
+            </h2>
+            <h3 className="text-lg text-rose-600 font-medium">Headboy</h3>
+            <div className="flex justify-center items-center relative gap-x-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"
+                className="w-4 fill-slate-50"
+              >
+                <path d="M224.2 89C216.3 70.1 195.7 60.1 176.1 65.4L170.6 66.9C106 84.5 50.8 147.1 66.9 223.3C104 398.3 241.7 536 416.7 573.1C493 589.3 555.5 534 573.1 469.4L574.6 463.9C580 444.2 569.9 423.6 551.1 415.8L453.8 375.3C437.3 368.4 418.2 373.2 406.8 387.1L368.2 434.3C297.9 399.4 241.3 341 208.8 269.3L253 233.3C266.9 222 271.6 202.9 264.8 186.3L224.2 89z" />
+              </svg>
+              <p className="text-sm sm:text-base font-medium text-slate-50">
+                0333 1234914
+              </p>
+            </div>
+            <div className="flex justify-center items-center relative gap-x-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"
+                className="w-4 fill-slate-50"
+              >
+                <path d="M112 128C85.5 128 64 149.5 64 176C64 191.1 71.1 205.3 83.2 214.4L291.2 370.4C308.3 383.2 331.7 383.2 348.8 370.4L556.8 214.4C568.9 205.3 576 191.1 576 176C576 149.5 554.5 128 528 128L112 128zM64 260L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 260L377.6 408.8C343.5 434.4 296.5 434.4 262.4 408.8L64 260z" />
+              </svg>
+              <p className="text-sm sm:text-base font-medium text-slate-50">
+                nameexam123@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </article>
   );
