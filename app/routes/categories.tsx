5ebd9979f12cc1cdee42197a5dad463ce7f21b60
Farjad <farjadabbas12@gmail.com>
2025-08-29 16:58:27 +0500

    .

--- app/routes/categories.tsx
+++ app/routes/categories.tsx
@@ -1,7 +1,10 @@
 function categories() {
   return (
-    <div>
-      <h1>Categories</h1>
+    <div className="pt-10 px-5">
+      <p className="text-2xl text-slate-300 mt-2 italic">
+        Categories will be uploaded in a few days. For now, they can be viewed
+        in the form
+      </p>
     </div>
   );
 }
--- app/routes/schedule.tsx
+++ app/routes/schedule.tsx
@@ -1,7 +1,11 @@
 function schedule() {
   return (
     <div>
-      <h1>Schedule</h1>
+      <div className="pt-10 px-5">
+        <p className="text-2xl text-slate-300 mt-2 italic">
+          Schedule will be uploaded a few weeks prior to the event.
+        </p>
+      </div>
     </div>
   );
 }
