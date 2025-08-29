25db8ca7df45d0f89dc2e48de43fc04cd8c69bdc
Farjad <farjadabbas12@gmail.com>
2025-08-29 16:52:36 +0500

    .

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -2,8 +2,9 @@ import { FilloutStandardEmbed } from "@fillout/react";
 
 function register() {
   return (
-    <div className="w-screen min-h-screen bg-[#13171A] flex flex-col justify-center">
-      <div>
+    <div className="w-screen flex items-center">
+      <div className="bg-[#13171A] absolute inset-0 z-0" />
+      <div className="pt-[10vh] relative z-10 flex-1 -translate-y-15">
         <FilloutStandardEmbed filloutId="mzPdn9juiXus" dynamicResize />
       </div>
     </div>
